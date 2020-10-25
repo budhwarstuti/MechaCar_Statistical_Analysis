50964e0e99669dba3d21e1ab99c28b359034b4a2
Stuti Budhwar <stutibudhwar31@gmail.com>
2020-10-25 12:12:46 -0700

    adding PSI /lot summary

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,10 @@ mecha_cars_df <- read.csv("MechaCar_mpg.csv", sep=",", header= TRUE) # adding cs
 head(mecha_cars_df) # testing the imported data
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mecha_cars_df) # applying linear regression for multiple points
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mecha_cars_df)) # arriving at summary
+
+#DELIVERABLE_2
+susp_coil_df <- read.csv("Suspension_coil.csv",sep = ",", header = TRUE) #adding csv file
+total_summary <- susp_coil_df %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep') # arriving at summary  
+lot_summary <- susp_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep') #Lot Summary DF
+
+
--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # MechaCar_Statistical_Analysis
 
 ## **Linear Regression to Predict MPG**
+**![Summary_Mechcar](./Summary_Mechcar.png)**
+
 * Which variables/coefficients provided a non-random amount of variance to the mpg values in the dataset?
 
 * Is the slope of the linear model considered to be zero? Why or why not?
+The slope is not considered to be zero because of the very samll p-value at 5.35e-11. We've got enough evidence to reject the null hypotheis.
 
 * Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
+This linear model predicts the mpg of MechaCar prototypes effectively. The multiple R-squared value of 0.7149 and the adjusted R-Squared value of 0.68 indicate  that there is a strong corelation between all factors provided with mpg.
